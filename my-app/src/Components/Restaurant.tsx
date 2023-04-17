4d625ad7fc367f823032a142cc34dddf8b16d168
Logan Levine <loganlev@udel.edu>
2023-04-17 16:51:46 -0400

    Made so I can edit component without changing app

---
+++ my-app/src/Components/Restaurant.tsx
@@ -0,0 +1,18 @@
+import React, { useState }  from "react";
+import { Button } from "react-bootstrap";
+
+export function Restaurant(): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(true);
+
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+    return (
+        <div>
+        <Button style = {{width: "50%", height: "10%", backgroundColor: "orange"}} onClick={() => console.log("Back")}>
+        Back
+      </Button> 
+    {"Website Name"}
+    </div>
+    );
+}

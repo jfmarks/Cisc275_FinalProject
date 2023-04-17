91ce675935e0dc04065d322115987dafbc7bd40d
jfmarks <jfmarks@udel.edu>
2023-04-17 19:15:27 -0400

    clearing the repo

--- src/Interfaces/DiningExperience.ts
+++
@@ -1,3 +0,0 @@
-export interface DiningExperience {
-    type: 'Fast Food' | 'Casual' | 'Luxury';
-}
--- src/Interfaces/MenuItem.ts
+++
@@ -1,6 +0,0 @@
-export interface MenuItem {
-    name: string;
-    description: string;
-    price: number;
-  }
-  

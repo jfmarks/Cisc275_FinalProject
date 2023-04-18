f76cdfdaa0e27db18ca0aed42eb2f8b782981a28
jfmarks <jfmarks@udel.edu>
2023-04-18 13:10:37 -0400

    Installed React dnd
    also added the HomePage component and added the Markup back into the repo

---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { Button, Row, Col } from "react-bootstrap";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider, useDrag } from "react-dnd";
+import { monitorEventLoopDelay } from "perf_hooks";
+import { isDecorator } from "typescript";
+{
+    /*
+export const ItemTypes = {
+    FOOD: "food"
+};
+
+function Food() {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: ItemTypes.FOOD,
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }));
+    return (
+        <div
+            ref={drag}
+            style={{
+                opacity: isDragging ? 0.5 : 1,
+                fontSize: 25,
+                fontWeight: "bold",
+                cursor: "move"
+            }}
+        >
+            bunger
+        </div>
+    );
+}
+*/
+}
+export function HomePage(): JSX.Element {
+    return (
+        <div>
+            <Row
+                style={{
+                    height: "100px",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    backgroundColor: "grey",
+                    margin: "20px",
+                    fontSize: 30
+                }}
+            >
+                WEBSITE NAME
+            </Row>
+            <Row>
+                <Col>
+                    <Row
+                        style={{
+                            width: "120px",
+                            height: "100px",
+                            verticalAlign: "center",
+                            backgroundColor: "grey",
+                            margin: "20px",
+                            fontSize: 30
+                        }}
+                    >
+                        List of Users
+                    </Row>
+                    <Row
+                        style={{
+                            width: "120px",
+                            height: "100px",
+                            verticalAlign: "center",
+                            margin: "20px",
+                            fontSize: 30
+                        }}
+                    >
+                        <Button
+                            style={{
+                                verticalAlign: "bottom",
+                                backgroundColor: "grey",
+                                marginTop: "5px"
+                            }}
+                        >
+                            Critic
+                        </Button>
+                        <Button
+                            style={{
+                                verticalAlign: "bottom",
+                                backgroundColor: "grey",
+                                marginTop: "5px"
+                            }}
+                        >
+                            Manager
+                        </Button>
+                        <Button
+                            style={{
+                                verticalAlign: "bottom",
+                                backgroundColor: "grey",
+                                marginTop: "5px"
+                            }}
+                        >
+                            Foodie
+                        </Button>
+                    </Row>
+                </Col>
+                {/*<Col>{Food()}</Col>*/}
+            </Row>
+        </div>
+    );
+}

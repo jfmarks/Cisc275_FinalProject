14ba7d6e7acf21775a9d603388930b7362dcdb5b
Alex <apeluso@udel.edu>
2023-04-20 14:48:36 -0400

    Condensed Interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -28,14 +28,6 @@ function App() {
             <p>
                 <Restaurant></Restaurant>
             </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
         </div>
     );
 }

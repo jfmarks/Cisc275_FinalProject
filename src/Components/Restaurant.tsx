04279a36845e47d2c217a20b8bb470ba368950bc
Logan Levine <loganlev@udel.edu>
2023-04-20 14:16:53 -0400

    restaurant component

--- src/Components/Restaurant.tsx
+++ src/Components/Restaurant.tsx
@@ -1,2 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
+
+export function Restaurant(): JSX.Element {
+    return (
+        <div>
+            <Row
+                style={{
+                    height: "100px",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    backgroundColor: "grey",
+                    margin: "20px",
+                    fontSize: 30
+                }}
+            >
+                Restaurant Name
+            </Row>
+            <Row>
+                <Col>
+                    <Row
+                        style={{
+                            width: "120px",
+                            height: "100px",
+                            verticalAlign: "center",
+                            backgroundColor: "grey",
+                            margin: "20px",
+                            fontSize: 30
+                        }}
+                    >
+                        List of Users
+                    </Row>
+                    <Row
+                        style={{
+                            width: "120px",
+                            height: "100px",
+                            verticalAlign: "center",
+                            margin: "20px",
+                            fontSize: 30
+                        }}
+                    >
+                        <Button
+                            style={{
+                                verticalAlign: "bottom",
+                                backgroundColor: "grey",
+                                marginTop: "5px"
+                            }}
+                        >
+                            Critic
+                        </Button>
+                        <Button
+                            style={{
+                                verticalAlign: "bottom",
+                                backgroundColor: "grey",
+                                marginTop: "5px"
+                            }}
+                        >
+                            Manager
+                        </Button>
+                        <Button
+                            style={{
+                                verticalAlign: "bottom",
+                                backgroundColor: "grey",
+                                marginTop: "5px"
+                            }}
+                        >
+                            Foodie
+                        </Button>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
+    );
+}

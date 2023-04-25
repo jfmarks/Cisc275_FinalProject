39b4abda775ec98c273b5dcbd6ed0fd94416612d
jfmarks <jfmarks@udel.edu>
2023-04-25 13:37:00 -0400

    Moved the contents of HomePage to App and cleaned it up

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,56 @@
 import React from "react";
 import "./App.css";
-import { HomePage } from "./Components/HomePage";
-import { Restaurant } from "./Components/Restaurant";
+//import { Restaurant } from "./Components/Restaurant";
 import { UserRestaurants } from "./Components/UserRestaurants";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">Team 6 Final</header>
-            <div
-                style={{
-                    textAlign: "left"
-                }}
-            >
-                <h1>Group Members</h1>
-                <ul>
-                    <li>Quinten Bettin</li>
-                    <li>Anastasia Donato</li>
-                    <li>Kareena Keswani</li>
-                    <li>Logan Levine</li>
-                    <li>Jacob Marks</li>
-                    <li>Alex Peluso</li>
-                </ul>
+            <h1 style={{ fontSize: 16 }}>
+                By Quinten Bettin, Anastasia Donato, Kareena Keswani, Logan
+                Levine, Jacob Marks, and Alex Peluso
+            </h1>
+            <div className="App">
+                <Container>
+                    <Row className="App-title">Rate-a-Restaurant</Row>
+                    <Row>
+                        <Col className="App-left-col" sm="auto">
+                            <Row className="App-main-button-rows">
+                                <Button
+                                    className="App-main-buttons"
+                                    style={{
+                                        height: "100px",
+                                        fontSize: 30,
+                                        marginTop: "-5px"
+                                    }}
+                                >
+                                    List of Users
+                                </Button>
+                            </Row>
+                            <Row className="App-main-button-rows">
+                                <Button className="App-main-buttons">
+                                    Critic
+                                </Button>
+                            </Row>
+                            <Row className="App-main-button-rows">
+                                <Button className="App-main-buttons">
+                                    Manager
+                                </Button>
+                            </Row>
+                            <Row className="App-main-button-rows">
+                                <Button className="App-main-buttons">
+                                    Foodie
+                                </Button>
+                            </Row>
+                        </Col>
+                        <Col>
+                            <UserRestaurants></UserRestaurants>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
-            <p>
-                <HomePage></HomePage>
-            </p>
-            <p>
-                <Restaurant></Restaurant>
-            </p>
-            <p>
-                <UserRestaurants></UserRestaurants>
-            </p>
+            {/*<Restaurant></Restaurant>*/}
         </div>
     );
 }

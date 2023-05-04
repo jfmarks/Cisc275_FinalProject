ebb6bfb097a3b9b96b37f0eff2f2f6d558a42adb
KAREENA <KAREENAK@UDEL.EDU>
2023-05-03 21:23:06 -0400

    slider imports

--- src/Components/Rating.tsx
+++ src/Components/Rating.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-/*
 import { Restaurant } from "../Interfaces";
 import RestaurantList from "./RestaurantList";
 import CurrentUser from "../CurrentUser";
-**/
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";

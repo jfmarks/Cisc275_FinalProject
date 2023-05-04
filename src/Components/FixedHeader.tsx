4078a627ab430425fff51b59bca6bfcc88a75de2
Alex <apeluso@udel.edu>
2023-05-04 15:10:47 -0400

    Added Drop Down Menu (Does not change CurrUser yet)

--- src/Components/FixedHeader.tsx
+++ src/Components/FixedHeader.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "../App.css";
+import CurrentUser from "../CurrentUser";
 
 const FixedHeader = () => {
     return (
         <header className="App-Fixed-Header">
-            <nav>
-                By Quinten Bettin, Anastasia Donato, Kareena Keswani, Logan
-                Levine, Jacob Marks, and Alex Peluso
-            </nav>
+            <label>
+                Select User Type:
+                <select>
+                    <option value="Critic">Critic</option>
+                    <option value="Manager">Manager</option>
+                    <option value="Foodie">Foodie</option>
+                </select>
+            </label>
         </header>
     );
 };

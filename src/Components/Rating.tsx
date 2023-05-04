a36768fc2f65b8ebc60f8401a3df496966e611bb
KAREENA <KAREENAK@UDEL.EDU>
2023-05-03 21:27:48 -0400

    fixing commit

--- src/Components/Rating.tsx
+++ src/Components/Rating.tsx
@@ -1,7 +1,6 @@
 import React from "react";
+/*
 import { Restaurant } from "../Interfaces";
 import RestaurantList from "./RestaurantList";
 import CurrentUser from "../CurrentUser";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+**/

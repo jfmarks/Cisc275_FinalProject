dee05aab8a45c1ad13b040a0d41421a969197863
KAREENA <KAREENAK@UDEL.EDU>
2023-05-03 23:14:08 -0400

    finished starter counter

--- src/Components/Rating.tsx
+++ src/Components/Rating.tsx
@@ -6,12 +6,31 @@ import CurrentUser from "../CurrentUser";
 
 export function Counter(): JSX.Element {
     const [value, setValue] = useState<number>(0);
+    function numbersToStars(num: number): string {
+        let stars = "";
+        if (value == 1) {
+            stars = "⭐️";
+        }
+        if (value == 2) {
+            stars = "⭐️⭐️";
+        }
+        if (value == 3) {
+            stars = "⭐️⭐️⭐️";
+        }
+        if (value == 4) {
+            stars = "⭐️⭐️⭐️⭐️";
+        }
+        if (value == 5) {
+            stars = "⭐️⭐️⭐️⭐️⭐️";
+        }
+        return stars;
+    }
     return (
         <span>
             <Button onClick={() => setValue(1 + value)} disabled={value === 5}>
                 Click to Add a Star
             </Button>
-            {value}
+            {numbersToStars}
         </span>
     );
 }

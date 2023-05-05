add94f04ad08c05cb1cd3f3f53662ec4ca0dc27c
Alex <apeluso@udel.edu>
2023-05-04 20:37:47 -0400

    Fixed unused vars errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from "react";
 import "./App.css";
 import "./RestaurantStyle.css";
 import { UserRestaurants } from "./Components/UserRestaurants";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import RestaurantBucket from "./Components/RestaurantBucket";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import CurrentUser from "./CurrentUser";
 import FixedHeader from "./Components/FixedHeader";
 
 function App() {

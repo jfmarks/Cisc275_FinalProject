588e6ddcf2b3fc81834d721f26e551491347b5cc
Logan Levine <loganlev@udel.edu>
2023-05-17 22:50:11 -0400

    fixed test files

--- src/Components/AddReview.test.tsx
+++ src/Components/AddReview.test.tsx
@@ -1,3 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ReviewDisplayElement from "./AddReview";
+
+describe("AddReview", () => {
+    test("renders the correct number of full stars", () => {
+        const { container } = render(
+            <ReviewDisplayElement
+                id={""}
+                restaurants={[]}
+                handleChange={function (): void {
+                    throw new Error("Function not implemented.");
+                }}
+            />
+        );
+        const starElements = container.getElementsByClassName("star");
+
+        expect(starElements.length).toBe(3);
+    });
+});
--- src/Components/RestaurantDrag.test.tsx
+++ src/Components/RestaurantDrag.test.tsx
@@ -1,3 +1,21 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import DragRestaurant from "./RestaurantDrag";
+import { DraggedRestaurant } from "../Interfaces";
+
+describe("RestaurantDrag", () => {
+    const temp: DraggedRestaurant = {
+        type: "",
+        id: "",
+        newItem: ""
+    };
+
+    test("renders the correct number of full stars", () => {
+        const { container } = render(
+            <DragRestaurant image={""} dragItem={temp} />
+        );
+        const starElements = container.getElementsByClassName("star");
+
+        expect(starElements.length).toBe(3);
+    });
+});

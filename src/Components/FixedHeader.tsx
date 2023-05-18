640043c2310ce7a1f966b3bb26a20e26a2564df7
KAREENA <KAREENAK@UDEL.EDU>
2023-05-17 23:23:32 -0400

    attempting to render search bar in header

--- src/Components/FixedHeader.tsx
+++ src/Components/FixedHeader.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../App.css";
 import { Button } from "react-bootstrap";
 import UserSelector from "./UserSelector";
+import SearchBar from "./SearchBar";
+import Restaurant from "./RestaurantList";
 
 const FixedHeader = () => {
     return (
@@ -9,6 +11,12 @@ const FixedHeader = () => {
             <Button style={{ position: "absolute", left: 5 }}>
                 List Of Users
             </Button>
+            <div style={{ position: "absolute", left: 5 }}>
+                <SearchBar
+                    placeholder="Do we have your restaurant?"
+                    data={Restaurant}
+                />
+            </div>
             <div style={{ position: "absolute", right: 5 }}>
                 <UserSelector></UserSelector>
             </div>
